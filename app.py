05a19a52dc896c8b9d830cad059eafb1e708648f
Eduard Ruzicka <edaruzicka@seznam.cz>
2023-08-15 10:13:56 +0200

    first commit to templates

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask in Docker!'
+def index():
+    return render_template('index.html')
 
 @app.route('/test')
 def fucku():
     return 'Hey, testing.'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
