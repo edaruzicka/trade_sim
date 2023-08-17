c794d16d97da499659d271b301eaf2db2373a97a
Eduard Ruzicka <edaruzicka@seznam.cz>
2023-08-17 16:08:46 +0200

    added sell/buy funcitonality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -7,35 +7,46 @@ from api_utils import get_current_sp500_price
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///trade_sim.db'
 db = SQLAlchemy(app)
+recreate_db = False
 
 class Trade_position(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    stock_name = db.Column(db.String(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    stock_ticker = db.Column(db.String(50), nullable=False)
     ammount_bought = db.Column(db.Integer, nullable=False)
     stock_price_at_buy = db.Column(db.Float, nullable=False)
     stock_price_at_sell = db.Column(db.Float, nullable=True)
-    date_buy = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    date_buy = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
     date_sell = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
         return f"<Bought_stock {self.stock_name}, {self.id}"
+    
+# RECREATE DB
+if recreate_db:
+    app.app_context().push()
+    db.drop_all()
+    db.create_all()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        new_position_price = request.form['price']
-        new_position = Trade_position(stock_price_at_buy=new_position_price)
+    trade_positions_opened = Trade_position.query.where(Trade_position.date_sell==None).order_by(Trade_position.date_buy).all()
+    trade_positions_closed = Trade_position.query.where(Trade_position.date_sell!=None).order_by(Trade_position.date_sell).all()
+    return render_template('index.html', trade_positions_opened=trade_positions_opened, trade_positions_closed=trade_positions_closed, api_data=True)
 
-        try:
-            db.session.add(new_position)
-            db.session.commit()
-            return Flask.redirect('/')
-        except:
-            return 'Opening new position failed'
-    else:
-        api_data = get_current_sp500_price()
-        trade_positions = Trade_position.query.order_by(Trade_position.date_buy).all()
-        return render_template('index.html', trade_positions=trade_positions)
+@app.route('/buy', methods=['POST'])
+def buy():
+    new_position = Trade_position(stock_ticker=request.form['stock_ticker'], ammount_bought=1, stock_price_at_buy=request.form['stock_price_current'])
+    db.session.add(new_position)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/sell', methods=['POST'])
+def sell():
+    position_to_sell = Trade_position.query.get(request.form['id'])
+    position_to_sell.stock_price_at_sell = request.form['stock_price_at_sell']
+    position_to_sell.date_sell = datetime.utcnow()
+    db.session.commit()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
