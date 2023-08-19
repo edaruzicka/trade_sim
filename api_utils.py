ca195d6352f14f062310f5775c591555e49143d4
Eduard Ruzicka <edaruzicka@seznam.cz>
2023-08-20 01:31:40 +0200

    crypto

--- api_utils.py
+++ api_utils.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -8,3 +9,22 @@ api_key = os.environ.get('API_KEY')
 headers = {
     'X-MBX-APIKEY': api_key,
 }
+
+def ping():
+    r = requests.get('https://api.binance.com/api/v3/time')
+    return r.content
+
+def get_top5_ticker_prices():
+    r = requests.get('https://api.binance.com/api/v3/ticker/price')
+    tickers_price = r.json()
+
+    selected_tickers = []
+    selected_tickers_json = {}
+    for ticker_price in tickers_price:
+        if ticker_price.get('symbol').endswith('USDT'):
+            selected_tickers.append(ticker_price)
+            selected_tickers_json[ticker_price.get('symbol')] = float(ticker_price.get('price'))
+        if len(selected_tickers) == 5:
+            break
+
+    return selected_tickers, selected_tickers_json
--- app.py
+++ app.py
@@ -2,24 +2,24 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-from api_utils import get_crypto_price
+from api_utils import ping, get_top5_ticker_prices
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///trade_sim.db'
 db = SQLAlchemy(app)
-recreate_db = False
+recreate_db = True
 
 class Trade_position(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    stock_ticker = db.Column(db.String(50), nullable=False)
+    crypto_ticker = db.Column(db.String(50), nullable=False)
     ammount_bought = db.Column(db.Integer, nullable=False)
-    stock_price_at_buy = db.Column(db.Float, nullable=False)
-    stock_price_at_sell = db.Column(db.Float, nullable=True)
+    crypto_price_at_buy = db.Column(db.Double(2), nullable=False)
+    crypto_price_at_sell = db.Column(db.Double(2), nullable=True)
     date_buy = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
     date_sell = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
-        return f"<Bought_stock {self.stock_name}, {self.id}"
+        return f"<Bought_crypto {self.crypto_name}, {self.id}"
     
 # RECREATE DB
 if recreate_db:
@@ -29,13 +29,14 @@ if recreate_db:
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    ticker_prices, ticker_prices_json = get_top5_ticker_prices()
     trade_positions_opened = Trade_position.query.where(Trade_position.date_sell==None).order_by(Trade_position.date_buy).all()
     trade_positions_closed = Trade_position.query.where(Trade_position.date_sell!=None).order_by(Trade_position.date_sell).all()
-    return render_template('index.html', trade_positions_opened=trade_positions_opened, trade_positions_closed=trade_positions_closed, api_data=True)
+    return render_template('index.html', trade_positions_opened=trade_positions_opened, trade_positions_closed=trade_positions_closed, ticker_prices=ticker_prices, ticker_prices_json=ticker_prices_json)
 
 @app.route('/buy', methods=['POST'])
 def buy():
-    new_position = Trade_position(stock_ticker=request.form['stock_ticker'], ammount_bought=1, stock_price_at_buy=request.form['stock_price_current'])
+    new_position = Trade_position(crypto_ticker=request.form['crypto_ticker'], ammount_bought=1, crypto_price_at_buy=request.form['crypto_price_current'])
     db.session.add(new_position)
     db.session.commit()
     return redirect('/')
@@ -43,10 +44,16 @@ def buy():
 @app.route('/sell', methods=['POST'])
 def sell():
     position_to_sell = Trade_position.query.get(request.form['id'])
-    position_to_sell.stock_price_at_sell = request.form['stock_price_at_sell']
+    position_to_sell.crypto_price_at_sell = request.form['crypto_price_at_sell']
     position_to_sell.date_sell = datetime.utcnow()
     db.session.commit()
     return redirect('/')
+
+@app.route('/api')
+def api_test():
+    #return ping()
+    text = get_top5_ticker_prices()
+    return render_template('sandbox.html', text=text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
